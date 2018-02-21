8906330b5e50a4c0229867e6109e3789b6cbf26e
Shalin <shalinpatel00@gmail.com>
2018-02-21 04:55:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function sayHiToGrandma(string) {
       return "YES INDEED!"
   } else if (string === "I love you, Grandma") {
   }   return "I love you too."
-    else {
+  }  else {
   }   return ""
 }
